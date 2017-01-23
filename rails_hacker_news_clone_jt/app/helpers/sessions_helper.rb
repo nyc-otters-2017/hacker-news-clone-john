afca1d1fe4383a5d519ace2778a622f3a4cd5f49
John Talio <johntalio@yahoo.com>
2017-01-23 15:51:10 -0500

    Fix typo

--- rails_hacker_news_clone_jt/app/helpers/sessions_helper.rb
+++ rails_hacker_news_clone_jt/app/helpers/sessions_helper.rb
@@ -5,11 +5,11 @@ module SessionsHelper
   end
 
   def log_in(user)
-    session[:user_id] = user_id
+    session[:user_id] = user.id
   end
 
   def logged_in?
     !current_user.nil?
   end
-  
+
 end
