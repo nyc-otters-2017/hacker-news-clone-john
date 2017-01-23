24a074488ab08f837698b60845155198a284f826
John Talio <johntalio@yahoo.com>
2017-01-23 14:43:00 -0500

    Beginning of user paths

--- rails_hacker_news_clone_jt/config/routes.rb
+++ rails_hacker_news_clone_jt/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+
+  get '/posts/new' => 'posts#new'
+  post '/posts' => 'posts#create', as: 'posts'
+  get '/posts/:id' => 'posts#show', as: 'post'
+  get '/posts/:id/edit' => 'posts#edit'
+
+  get '/users/new' => 'users#new'
 end
