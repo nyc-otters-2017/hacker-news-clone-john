3abd0ade44f013851752a556adba0e89828b53fb
John Talio <johntalio@yahoo.com>
2017-01-23 18:08:48 -0500

    Making it look pretty

--- rails_hacker_news_clone_jt/app/controllers/posts_controller.rb
+++ rails_hacker_news_clone_jt/app/controllers/posts_controller.rb
@@ -24,7 +24,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
   private
 
