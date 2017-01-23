e5e84ce20562d3d3cfc97ba104b79e7ad2298195
John Talio <johntalio@yahoo.com>
2017-01-23 15:50:58 -0500

    fixing redirect paths

--- rails_hacker_news_clone_jt/app/controllers/users_controller.rb
+++ rails_hacker_news_clone_jt/app/controllers/users_controller.rb
@@ -10,10 +10,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 
   private
